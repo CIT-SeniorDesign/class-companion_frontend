56b0f1f0c3c56b1c0749d5649f68fc4d567f1046
jonjosh <37912467+jonjosh@users.noreply.github.com>
2021-03-07 12:52:02 -0800

    Added Hind and Montserrat as a fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'hind': ['Hind'],
+      'montserrat': ['Montserrat']
+    },
     extend: {},
   },
   variants: {
